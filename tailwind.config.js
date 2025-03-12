66b4474be00db333615744c94559886d9195b5de
Qamrul-Hassan <sajal700@gmail.com>
2025-03-13 01:15:54 +0600

    Initial commit with Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"], // Tailwind will scan HTML files here
+  theme: {
+    extend: {}, // Add custom styles here later if needed
+  },
+  plugins: [],
+};
